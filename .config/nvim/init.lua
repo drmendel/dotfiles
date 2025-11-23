2a405f7ade9495fa4d98aa5997994dee38d1e4ea
Dobondi-Reisz Mendel <dobondireiszmendel@gmail.com>
2025-11-23 11:25:59 +0100

    Add plugins.visuals.colorizer to nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,5 +11,6 @@ require("lazy").setup({
   require("plugins.utils.vimbegood"),
   require("plugins.utils.vimfugitive"),
   require("plugins.utils.whichkey"),
-  require("plugins.visuals.catppuccin")
+  require("plugins.visuals.catppuccin"),
+  require("plugins.visuals.colorizer")
 })

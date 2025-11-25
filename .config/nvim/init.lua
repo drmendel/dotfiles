aa8dc868f3c3d841c24d3ddb8721563c725d49a8
Dobondi-Reisz Mendel <dobondireiszmendel@gmail.com>
2025-11-25 13:46:46 +0100

    Add plugins.utils.autoformat to nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,7 @@ require('core.lazy')
 require('custom.terminal')
 
 require("lazy").setup({
+  require("plugins.utils.autoformat"),
   require("plugins.utils.autopairs"),
   require("plugins.utils.completion"),
   require("plugins.utils.lsp"),
